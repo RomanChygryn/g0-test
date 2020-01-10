6a6a730bf5c9d37b7bbd08204c687c240a3ada93
r.chygryn <r.chygryn@cheil.com>
2020-01-10 09:59:02 +0100

    clean app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('g0-test app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Main Window');
   });
 });
