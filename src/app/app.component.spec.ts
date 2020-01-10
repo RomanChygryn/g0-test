5d49046282fc39fa5afebccd4b4535ea2829f03c
r.chygryn <r.chygryn@cheil.com>
2020-01-10 08:32:21 +0100

    new check, should be no error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'g0-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('g0-test');
+    expect(app.title).toEqual('g0-test-app');
   });
 
   it('should render title', () => {
